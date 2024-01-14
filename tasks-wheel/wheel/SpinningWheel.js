21dfad7d3ba7f823fee291a2eb231b995c272a2e
Krystle Mitchell <mitchell.krystle2@gmail.com>
2024-01-14 12:13:06 -0800

    Wheel Is divided

--- tasks-wheel/wheel/SpinningWheel.js
+++ tasks-wheel/wheel/SpinningWheel.js
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import Svg, { Circle, Text as SvgText } from 'react-native-svg';
+import Svg, { Circle, Text as SvgText, G, Path } from 'react-native-svg';
 import {
   GestureHandlerRootView,
   PanGestureHandler,
@@ -12,22 +12,65 @@ const SpinningWheel = () => {
 
   const onWheelPan = (event) => {};
 
+  const launchSegments = () => {
+    const numOfSegments = 5;
+    const angle = (2 * Math.PI) / numOfSegments;
+
+    const segments = [];
+    for (let i = 0; i < numOfSegments; i++) {
+      const startAngle = i * angle;
+      const endAngle = (i + 1) * angle;
+
+      const pathData = `
+        M 150 150 
+        L ${150 + 140 * Math.cos(startAngle)} ${
+        150 + 140 * Math.sin(startAngle)
+      }
+        A 140 140 0 0 1 ${150 + 140 * Math.cos(endAngle)} ${
+        150 + 140 * Math.sin(endAngle)
+      }
+        Z
+        `;
+
+      segments.push(
+        <Path
+          d={pathData}
+          cx={150}
+          cy={150}
+          r={140}
+          fill={`hsl(${(i * 360) / numOfSegments}, 50%, 70%)`}
+        />
+      );
+    }
+    return segments;
+  };
+
   return (
     <GestureHandlerRootView>
-      <View stlye={styles.container}>
+      <View style={styles.container}>
         <PanGestureHandler onGestureEvent={onWheelPan}>
           <Svg width={300} height={300}>
-            <Circle cx={150} cy={150} r={140} fill="yellow" />
-            <SvgText
-              x={150}
-              y={150}
-              fontSize={20}
-              textAnchor="middle"
-              fill="black"
-              top={50}
-            >
-              Spin me!
-            </SvgText>
+            <G>
+              {launchSegments()}
+              <Circle
+                cx={150}
+                cy={150}
+                r={55}
+                fill="grey"
+                stroke="white"
+                strokeWidth={1.5}
+              />
+              <SvgText
+                x={150}
+                y={150}
+                fontSize={20}
+                textAnchor="middle"
+                fill="white"
+                top={19}
+              >
+                Spin me!
+              </SvgText>
+            </G>
           </Svg>
         </PanGestureHandler>
         <TouchableOpacity onPress={() => onSpinPress('Default Task')}>
@@ -57,4 +100,3 @@ const styles = StyleSheet.create({
 });
 
 export default SpinningWheel;
-
